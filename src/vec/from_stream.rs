5376b55a45f15d012b55547c3315ec30bb680f7b
Yoshua Wuyts <yoshuawuyts@gmail.com>
2019-08-30 12:20:01 +0200

    remove macro from vec::from_stream
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- src/vec/from_stream.rs
+++ src/vec/from_stream.rs
@@ -1,27 +1,12 @@
-use cfg_if::cfg_if;
-
 use crate::stream::{FromStream, IntoStream, Stream};
 
 use std::pin::Pin;
 
-cfg_if! {
-    if #[cfg(feature = "docs")] {
-        #[doc(hidden)]
-        pub struct DynFuture<'a, T>(std::marker::PhantomData<&'a T>);
-
-        macro_rules! dyn_ret {
-            ($a:lifetime, $o:ty) => (DynFuture<$a, $o>);
-        }
-    } else {
-        macro_rules! dyn_ret {
-            ($a:lifetime, $o:ty) => (Pin<Box<dyn core::future::Future<Output = $o> + Send + 'a>>)
-        }
-    }
-}
-
 impl<T: Unpin + Sized + Send> FromStream<T> for Vec<T> {
     #[inline]
-    fn from_stream<'a, S: IntoStream<Item = T>>(stream: S) -> dyn_ret!('a, Self)
+    fn from_stream<'a, S: IntoStream<Item = T>>(
+        stream: S,
+    ) -> Pin<Box<dyn core::future::Future<Output = Self> + Send + 'a>>
     where
         <S as IntoStream>::IntoStream: Send + 'a,
     {
